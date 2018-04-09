fa8004b698e7c698216cdd9f03415328bf245d21
CrzyBear <1798900899@qq.com>
2018-04-09 17:49:44 +0800

    com/javase/设计模式/模板模式

---
+++ src/main/java/com/javase/设计模式/模板模式/NodeTemplate.java
@@ -0,0 +1,18 @@
+package com.javase.设计模式.模板模式;
+
+public class NodeTemplate extends Template{
+    @Override
+    protected void doBefore() {
+        System.out.println("煮面");
+    }
+
+    @Override
+    protected void doing() {
+        System.out.println("吃面");
+    }
+
+    @Override
+    protected void doAfter() {
+        System.out.println("洗碗");
+    }
+}
